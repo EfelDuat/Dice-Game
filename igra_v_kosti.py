1783aea38fa9b628a804d1aa87c899e4d28937fc
EfelDuat <kornakov2010@yandex.ru>
2025-09-28 22:50:20 +0300

    Add files via upload

---
+++ igra_v_kosti.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+def dice():
+    print("Добро пожаловать в игру в кости!")
+    
+    while True:
+        # Бросок игрока
+        player = random.randint(1, 6)
+        print("\nВаш бросок:")
+        print("У вас выпало", player)
+        
+        # Бросок компьютера
+        print("\nКомпьютер делает бросок...")
+        time.sleep(2)
+        ai = random.randint(1, 6)
+        print("У компьютера выпало", ai)
+        
+        # Определение победителя
+        if player > ai:
+            print("\nВы победили!")
+        elif player == ai:
+            print("\nНичья!")
+        else:
+            print("\nВы проиграли!")
+        
+        # Вопрос о продолжении игры
+        while True:
+            choice = input("\nХотите сыграть еще раз? (Y/N): ").strip().upper()
+            if choice in ["Y", "N"]:
+                break
+            print("Неверный ввод. Пожалуйста, введите Y или N.")
+        
+        if choice == "N":
+            print("Спасибо за игру! До свидания!")
+            break
+
+if __name__ == "__main__":
+    dice()
+
